22713906ccfbad4a145bf0803d8e054a4f918435
Jack Wu <jackwu350@yahoo.com>
2017-10-31 17:50:37 -0700

    sessions

--- RedditClone/Reddit/app/controllers/posts_controller.rb
+++ RedditClone/Reddit/app/controllers/posts_controller.rb
@@ -1,2 +1,18 @@
 class PostsController < ApplicationController
+  def new
+
+
+  end  
+
+
+  def create
+  @post = Post.create(post_params)
+
+  if @post.save
+
+  else
+    flash[:errors] = @post.errors.full_messages
+
+  end
+
 end
