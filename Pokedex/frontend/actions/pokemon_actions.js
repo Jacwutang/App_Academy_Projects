9552012f111aa5e23badf820a69e264859c9e0a6
Anna <annaoh1994@gmail.com>
2017-11-15 17:58:50 -0800

    finished Phase 1 and 2  of Pokemon

---
+++ Pokedex/frontend/actions/pokemon_actions.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { fetchAllPokemon } from '../util/api_util';
+
+export const RECEIVE_ALL_POKEMON = "RECEIVE_ALL_POKEMON";
+
+export const receiveAllPokemon = (pokemon) => ({
+  type: "RECEIVE_ALL_POKEMON",
+  pokemon
+});
+
+
+export const requestAllPokemon = () => (dispatch) => (
+  fetchAllPokemon()
+  .then(pokemon => dispatch(receiveAllPokemon(pokemon)))
+
+
+
+);
---
+++ Pokedex/frontend/reducers/entities_reducer.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+
+import pokemonReducer from './pokemon_reducer';
+
+
+export default combineReducers({
+  pokemon: pokemonReducer
+});
